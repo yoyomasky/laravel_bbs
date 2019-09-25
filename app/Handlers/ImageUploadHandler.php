6d2d65ed9eb945434c4eedac75c54c3694ca62eb
yoyomasky <yoyomasky@gmail.com>
2019-09-25 14:41:14 +0800

    modify

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,14 +2,23 @@
 
 namespace App\Handlers;
 
-use  Illuminate\Support\Str;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
     // 只允许以下后缀名的图片文件上传
     protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    /**
+     * 上传图片方法
+     * @param [type] $file
+     * @param [type] $folder
+     * @param [type] $file_prefix
+     * @param boolean $max_width
+     * @return void
+     */
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
         // 文件夹切割能让查找效率更高。
@@ -27,15 +36,45 @@ class ImageUploadHandler
         $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
 
         // 如果上传的不是图片将终止操作
-        if ( ! in_array($extension, $this->allowed_ext)) {
+        if (!in_array($extension, $this->allowed_ext)) {
             return false;
         }
 
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $filename);
 
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+    /**
+     * 裁剪图片方法
+     * @param [type] $file_path
+     * @param [type] $max_width
+     * @return void
+     */
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
