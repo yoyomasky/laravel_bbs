c44d63b181ff52f91825ce215295b3ad879f5414
yoyomasky <yoyomasky@gmail.com>
2019-09-26 15:35:53 +0800

    modify

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-         $this->call(UsersTableSeeder::class);
-		//$this->call(TopicsTableSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call(UsersTableSeeder::class);
+    $this->call(TopicsTableSeeder::class);
+  }
 }
