7ffe78aa15a135bb7ab9cf89ca5a8736f859d334
yoyomasky <yoyomasky@gmail.com>
2019-09-29 15:11:43 +0800

    modify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'McBBS') - Laravel框架</title>
+  <meta name="description" content="@yield('description', 'MCBBS 认真学习~')" />
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
