f619456a18a2bcaaa23ddaf10ac10ace18b8e327
yoyomasky <yoyomasky@gmail.com>
2019-09-29 12:40:10 +0800

    modify

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -6,36 +6,31 @@ class TopicRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
+        switch ($this->method()) {
+                // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
+                // UPDATE
             case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
+            case 'PATCH': {
+                    return [
+                        'title'       => 'required|min:2',
+                        'body'        => 'required|min:3',
+                        'category_id' => 'required|numeric',
+                    ];
+                }
             case 'GET':
             case 'DELETE':
-            default:
-            {
-                return [];
-            };
+            default: {
+                    return [];
+                };
         }
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
